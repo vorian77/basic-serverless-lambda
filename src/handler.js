07a29a7593d73c9108be48ed55a0bf38d891b27e
Phyllip Hall <phall776@gmail.com>
2022-08-02 16:02:09 -0400

    handler function name didn't update in GitHub

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.generateRamdonNumber = event => {
+module.exports.generateRandomNumber = event => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
